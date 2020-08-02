49625e7a28138e888adf32d75e64867b91746d98
Nate Lee <nateleee.dev@gmail.com>
2020-08-02 20:15:31 +0800

    - [x] Integrating Core Image with SwiftUI.

--- Instafilter/Views/ContentView.swift
+++ Instafilter/Views/ContentView.swift
@@ -26,30 +26,41 @@ struct ContentView: View {
     func loadImage() {
         guard let inputImage = UIImage(named: "example") else { return }
         let beginImage = CIImage(image: inputImage)
-
+        
         let context = CIContext()
         
-//        let currentFilter = CIFilter.sepiaTone()
-//        currentFilter.inputImage = beginImage
-//        currentFilter.intensity = 0.5
+        //        let currentFilter = CIFilter.sepiaTone()
+        //        currentFilter.inputImage = beginImage
+        //        currentFilter.intensity = 0.5
         
-//        let currentFilter = CIFilter.pixellate()
-//        currentFilter.inputImage = beginImage
-//        currentFilter.scale = 100
+        //        let currentFilter = CIFilter.pixellate()
+        //        currentFilter.inputImage = beginImage
+        //        currentFilter.scale = 100
         
-        let currentFilter = CIFilter.crystallize()
-        // currentFilter.inputImage = beginImage // this will CRASH!
+        //        let currentFilter = CIFilter.crystallize()
+        //        // currentFilter.inputImage = beginImage // this will CRASH!
+        //        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        //        currentFilter.radius = 90
+        
+        guard let currentFilter = CIFilter(name: "CITwirlDistortion") else { return }
         currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
-        currentFilter.radius = 90
+        currentFilter.setValue(1200, forKey: kCIInputRadiusKey)
+        currentFilter.setValue(
+            CIVector(
+                x: inputImage.size.width / 2,
+                y: inputImage.size.height / 2
+            ),
+            forKey: kCIInputCenterKey
+        )
         
         // get a CIImage from our filter or exit if that fails
         guard let outputImage = currentFilter.outputImage else { return }
-
+        
         // attempt to get a CGImage from our CIImage
         if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
             // convert that to a UIImage
             let uiImage = UIImage(cgImage: cgimg)
-
+            
             // and convert that to a SwiftUI image
             image = Image(uiImage: uiImage)
         }
