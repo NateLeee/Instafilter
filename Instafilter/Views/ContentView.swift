47005eed74a2246b4f84e2a61d29d0a0e075eb70
Nate Lee <nateleee.dev@gmail.com>
2020-08-02 20:04:40 +0800

    Renamed, i.e. copied the code from the site.

--- Instafilter/Views/ContentView.swift
+++ Instafilter/Views/ContentView.swift
@@ -24,22 +24,24 @@ struct ContentView: View {
     }
     
     func loadImage() {
-        // image = Image("example")
-        
-        guard let inputUIImage = UIImage(named: "example") else {
-            return
-        }
-        let ciImage = CIImage(image: inputUIImage)
-        
+        guard let inputImage = UIImage(named: "example") else { return }
+        let beginImage = CIImage(image: inputImage)
+
         let context = CIContext()
-        let filter = CIFilter.sepiaTone()
+        let currentFilter = CIFilter.sepiaTone()
         
-        filter.inputImage = ciImage
-        filter.intensity = 0.9
+        currentFilter.inputImage = beginImage
+        currentFilter.intensity = 1
         
-        guard let outputCIImage = filter.outputImage else { return }
-        if let outputCGImage = context.createCGImage(outputCIImage, from: outputCIImage.extent) {
-            let uiImage = UIImage(cgImage: outputCGImage)
+        // get a CIImage from our filter or exit if that fails
+        guard let outputImage = currentFilter.outputImage else { return }
+
+        // attempt to get a CGImage from our CIImage
+        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+            // convert that to a UIImage
+            let uiImage = UIImage(cgImage: cgimg)
+
+            // and convert that to a SwiftUI image
             image = Image(uiImage: uiImage)
         }
     }
