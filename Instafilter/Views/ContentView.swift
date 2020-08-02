06c112bd2c38b6b699bc32e73a24c470db6c77fb
Nate Lee <nateleee.dev@gmail.com>
2020-08-02 10:08:18 +0800

    Sadly this code won't work!

--- Instafilter/Views/ContentView.swift
+++ Instafilter/Views/ContentView.swift
@@ -11,18 +11,17 @@ import SwiftUI
 struct ContentView: View {
     @State private var blurAmount: CGFloat = 0
     
+    let blurBinding = Binding<CGFloat>(
+        get: {
+            self.blurAmount
+        },
+        set: {
+            self.blurAmount = $0
+            print("New value is \(self.blurAmount)")
+        }
+    )
     
     var body: some View {
-        let blurBinding = Binding<CGFloat>(
-            get: {
-                self.blurAmount
-            },
-            set: {
-                self.blurAmount = $0
-                print("New value is \(self.blurAmount)")
-            }
-        )
-        
         return VStack {
             Text("Hello, World!")
                 .blur(radius: blurAmount)
