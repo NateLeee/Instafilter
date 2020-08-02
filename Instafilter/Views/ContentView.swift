8d8dd25bef1d5e48e4e13c4a123ca634fab74c3d
Nate Lee <nateleee.dev@gmail.com>
2020-08-02 19:01:24 +0800

    - [x] Creating custom bindings in SwiftUI.

--- Instafilter/Views/ContentView.swift
+++ Instafilter/Views/ContentView.swift
@@ -11,17 +11,16 @@ import SwiftUI
 struct ContentView: View {
     @State private var blurAmount: CGFloat = 0
     
-    let blurBinding = Binding<CGFloat>(
-        get: {
-            self.blurAmount
-        },
-        set: {
-            self.blurAmount = $0
-            print("New value is \(self.blurAmount)")
-        }
-    )
-    
     var body: some View {
+        let blurBinding = Binding<CGFloat>(
+            get: {
+                self.blurAmount      
+        },
+            set: { (newBlurAmount) in
+                self.blurAmount = newBlurAmount
+                print("New value is \(self.blurAmount)")
+        })
+        
         return VStack {
             Text("Hello, World!")
                 .blur(radius: blurAmount)
