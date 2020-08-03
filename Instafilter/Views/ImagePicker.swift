f6e0ecc8e2721f5cf611ca9787e3102b2fad8bc4
Nate Lee <nateleee.dev@gmail.com>
2020-08-03 19:28:55 +0800

    Added a new swift file: ImagePicker.

---
+++ Instafilter/Views/ImagePicker.swift
@@ -0,0 +1,23 @@
+//
+//  ImagePicker.swift
+//  Instafilter
+//
+//  Created by Nate Lee on 8/3/20.
+//  Copyright © 2020 Nate Lee. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    typealias UIViewControllerType = UIImagePickerController
+    
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        <#code#>
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+        <#code#>
+    }
+    
+}
