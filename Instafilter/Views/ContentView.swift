7624938664bb256d32f4b5e32943f55d844864d7
Nate Lee <nateleee.dev@gmail.com>
2020-08-03 12:59:15 +0800

    Restored all the codes.

--- Instafilter/Views/ContentView.swift
+++ Instafilter/Views/ContentView.swift
@@ -7,52 +7,18 @@
 //
 
 import SwiftUI
-import CoreImage
-import CoreImage.CIFilterBuiltins
 
 struct ContentView: View {
-    @State private var image: Image?
-    @State private var inputImage: UIImage?
-    @State private var showingImagePicker = false
+    
     
     var body: some View {
         VStack {
-            image?
-                .resizable()
-                .scaledToFit()
-            
-            Button("Select Image") {
-                self.showingImagePicker = true
-            }
+            Text("Hello!")
         }
-        .sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
-            ImagePicker(image: self.$inputImage)
-        }
-        
-    }
-    
-    func loadImage() {
-        guard let inputImage = inputImage else {
-            return
-        }
-        self.image = Image(uiImage: inputImage)
-        
-        // Save image to the photo library
-        let imageSaver = ImageSaver()
-        imageSaver.saveToPhotoLibrary(uiImage: inputImage)
     }
     
 }
 
-class ImageSaver: NSObject {
-    func saveToPhotoLibrary(uiImage: UIImage) {
-        UIImageWriteToSavedPhotosAlbum(uiImage, self, #selector(saveError), nil)
-    }
-    
-    @objc func saveError(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
-        print("Saved!")
-    }
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
