148e1b3181660f5bf48ca45c3e506a4960717d98
Nate Lee <nateleee.dev@gmail.com>
2020-08-04 11:54:59 +0800

    Added a new file: ImageSaver.

---
+++ Instafilter/Helpers/ImageSaver.swift
@@ -0,0 +1,13 @@
+//
+//  ImageSaver.swift
+//  Instafilter
+//
+//  Created by Nate Lee on 8/4/20.
+//  Copyright © 2020 Nate Lee. All rights reserved.
+//
+
+import UIKit
+
+class ImageSaver: NSObject {
+    
+}
